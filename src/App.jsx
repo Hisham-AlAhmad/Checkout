6844dade2675987a0a60828cda1277cd8b5a3bb0
Ibrahim Zaher Zeidan <himazeidan94@gmail.com>
2026-01-26 14:19:15 +0200

    contact page created

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import { useState } from 'react'
 import './App.css'
+import Contact from './components/Contact.jsx'
 function App() {
     return(
         <>
+        <Contact/>
         </>
     );
 }
