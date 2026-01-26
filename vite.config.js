9689015510e1baf93b989607789123943fadb524
Hisham Al Ahmad <hisham000x@gmail.com>
2026-01-26 11:43:56 +0200

    Updating vite so that it will render on every change

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,10 @@ import tailwind from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(), tailwind()],
+  base: './',
+  server: {
+    watch: {
+      usePolling: true,
+    }
+  },
 })
