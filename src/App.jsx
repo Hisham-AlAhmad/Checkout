2fa0f91f4c41ba797c42374adf48ba509597ed84
Hisham Al Ahmad <hisham000x@gmail.com>
2026-01-26 13:02:40 +0200

    Making the body bg gray

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,10 @@ import Navbar from './components/Navbar';
 import Steps from './components/Steps';
 function App() {
     return(
-        <>
+        <div className="min-h-screen bg-gray-100">
         <Navbar/>
         <Steps/>
-        </>
+        </div>
     );
 }
 export default App
